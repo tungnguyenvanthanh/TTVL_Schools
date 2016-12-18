97fdcdacded5c62fc8785c4aa724360063e41749
tungnguyenvanthanh <tungnguyenvanthanh@hotmail.com>
2016-12-19 00:19:24 +0700

    fff
    fff

---
+++ Code/OnlineShop/Models/AccountModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models.Framework;
+
+namespace Models
+{
+    public class AccountModel
+    {
+        private OnlineShopDbContext db = null;
+        public AccountModel() { db = new OnlineShopDbContext();}
+
+        public bool Login(string username, string password)
+        {
+            object[] sqlParamater = 
+            {
+                new SqlParameter("@UserName", username),
+                new SqlParameter("@Password", password)
+            };
+            var res = db.Database.SqlQuery<bool>("Account_Login @UserName, @Password", sqlParamater).SingleOrDefault();
+            return res;
+        }
+    }
+}
---
+++ Code/OnlineShop/OnlineShop/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Web;
+using System.Web.Mvc;
+using Models;
+using OnlineShop.Areas.Admin.Code;
+using OnlineShop.Areas.Admin.Models;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin/Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(LoginModel model)
+        {
+            if (model.UserName == null)
+            {
+                ModelState.AddModelError("", "[Tài khoản] không được để tróng");
+                return View(model);
+            }
+            else if (model.Password == null)
+            {
+                ModelState.AddModelError("", "[Mật khẩu] không được để tróng");
+                return View(model);
+            }
+            else
+            {
+                var result = new AccountModel().Login(model.UserName, model.Password);
+                if (result && ModelState.IsValid)
+                {
+                    SessionHelper.SetSession(new UserSession() { UserName = model.UserName });
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "[Tài khoản] hoặc [Mật khẩu] không đúng.");
+                }
+                return View(model);
+            }
+        }
+    }
+}
